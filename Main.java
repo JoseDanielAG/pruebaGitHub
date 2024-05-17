351826fe2ca4c1e6fc82de7c14e6d3f2d733ff61
joseDaniel <argon181120@gmail.com>
2024-05-17 20:08:00 +0200

    primera foto. Clases Main y CCuenta

---
+++ Main.java
@@ -0,0 +1,29 @@
+
+package cuentas;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        operaciones();
+ }
+
+    private static void operaciones() {
+        CCuenta cuenta1;
+        double saldoActual;
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es "+ saldoActual );
+        // Operaciones de ingreso y retiro
+        cuenta1.retirar(2300);
+        cuenta1.ingresar(685);
+        
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es "+ saldoActual );
+    }
+
+    }
+        
+    
+    
+
